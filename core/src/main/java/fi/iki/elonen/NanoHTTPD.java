e53bf56d480775a7e9a0c2bffad1a3026e585cc9
Philip Taron [Qumulo] <philip@qumulo.com>
2016-02-05 15:13:08 -0800

    Move the IHTTPSession interface to the top.
    This is part of a series of patches moving the interfaces to the beginning of
    the file.

--- core/src/main/java/fi/iki/elonen/NanoHTTPD.java
+++ core/src/main/java/fi/iki/elonen/NanoHTTPD.java
@@ -150,6 +150,54 @@ import fi.iki.elonen.NanoHTTPD.Response.Status;
 public abstract class NanoHTTPD {
 
     /**
+     * Handles one session, i.e. parses the HTTP request and returns the
+     * response.
+     */
+    public interface IHTTPSession {
+
+        void execute() throws IOException;
+
+        CookieHandler getCookies();
+
+        Map<String, String> getHeaders();
+
+        InputStream getInputStream();
+
+        Method getMethod();
+
+        Map<String, String> getParms();
+
+        String getQueryParameterString();
+
+        /**
+         * @return the path part of the URL.
+         */
+        String getUri();
+
+        /**
+         * Adds the files in the request body to the files map.
+         * 
+         * @param files
+         *            map to modify
+         */
+        void parseBody(Map<String, String> files) throws IOException, ResponseException;
+
+        /**
+         * Get the remote ip address of the requester.
+         * 
+         * @return the IP address.
+         */
+        String getRemoteIpAddress();
+
+        /**
+         * Get the remote hostname of the requester.
+         * 
+         * @return the hostname.
+         */
+        String getRemoteHostName();
+    }
+
+    /**
      * Pluggable strategy for asynchronously executing requests.
      */
     public interface AsyncRunner {
@@ -1140,54 +1188,6 @@ public abstract class NanoHTTPD {
         public String getRemoteHostName() {
             return this.remoteHostname;
         }
-    }
-
-    /**
-     * Handles one session, i.e. parses the HTTP request and returns the
-     * response.
-     */
-    public interface IHTTPSession {
-
-        void execute() throws IOException;
-
-        CookieHandler getCookies();
-
-        Map<String, String> getHeaders();
-
-        InputStream getInputStream();
-
-        Method getMethod();
-
-        Map<String, String> getParms();
-
-        String getQueryParameterString();
-
-        /**
-         * @return the path part of the URL.
-         */
-        String getUri();
-
-        /**
-         * Adds the files in the request body to the files map.
-         * 
-         * @param files
-         *            map to modify
-         */
-        void parseBody(Map<String, String> files) throws IOException, ResponseException;
-
-        /**
-         * Get the remote ip address of the requester.
-         * 
-         * @return the IP address.
-         */
-        String getRemoteIpAddress();
-
-        /**
-         * Get the remote hostname of the requester.
-         * 
-         * @return the hostname.
-         */
-        String getRemoteHostName();
     }
 
     /**
