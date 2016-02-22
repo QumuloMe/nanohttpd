538924cc503311f7cb9821e8e8acde9c737cbe42
Philip Taron (@Qumulo) <philip@qumulo.com>
2016-02-22 11:32:23 -0800

    Extract code which throws IOError into its own method. Remove big `try`

--- core/src/main/java/fi/iki/elonen/NanoHTTPD.java
+++ core/src/main/java/fi/iki/elonen/NanoHTTPD.java
@@ -875,91 +875,93 @@ public abstract class NanoHTTPD {
                         + "but it contains less than two boundary strings.");
             }
 
-            try {
-                int pcount = 0;
-                byte[] partHeaderBuff = new byte[MAX_HEADER_SIZE];
-                for (int boundaryIdx = 0; boundaryIdx < boundaryIdxs.length - 1; boundaryIdx++) {
-                    fbuf.position(boundaryIdxs[boundaryIdx]);
-                    int len = (fbuf.remaining() < MAX_HEADER_SIZE) ? fbuf.remaining() : MAX_HEADER_SIZE;
-                    fbuf.get(partHeaderBuff, 0, len);
-                    BufferedReader in =
-                            new BufferedReader(new InputStreamReader(new ByteArrayInputStream(partHeaderBuff, 0, len), Charset.forName(contentType.getEncoding())), len);
+            int pcount = 0;
+            byte[] partHeaderBuff = new byte[MAX_HEADER_SIZE];
+            for (int boundaryIdx = 0; boundaryIdx < boundaryIdxs.length - 1; boundaryIdx++) {
+                fbuf.position(boundaryIdxs[boundaryIdx]);
+                int len = (fbuf.remaining() < MAX_HEADER_SIZE) ? fbuf.remaining() : MAX_HEADER_SIZE;
+                fbuf.get(partHeaderBuff, 0, len);
+                BufferedReader in =
+                        new BufferedReader(new InputStreamReader(new ByteArrayInputStream(partHeaderBuff, 0, len), Charset.forName(contentType.getEncoding())), len);
 
-                    int headerLines = 0;
-                    // First line is boundary string
-                    String mpline = in.readLine();
-                    headerLines++;
-                    if (mpline == null || !mpline.contains(contentType.getBoundary())) {
-                        throw new ResponseException(Response.Status.BAD_REQUEST, "BAD REQUEST: Content type is multipart/form-data but chunk does not start with boundary.");
-                    }
+                int headerLines = 0;
+                // First line is boundary string
+                String mpline = getNextHeaderLine(in);
+                headerLines++;
+                if (mpline == null || !mpline.contains(contentType.getBoundary())) {
+                    throw new ResponseException(Response.Status.BAD_REQUEST, "BAD REQUEST: Content type is multipart/form-data but chunk does not start with boundary.");
+                }
 
-                    String partName = null, fileName = null, partContentType = null;
-                    // Parse the reset of the header lines
-                    mpline = in.readLine();
-                    headerLines++;
-                    while (mpline != null && mpline.trim().length() > 0) {
-                        Matcher matcher = CONTENT_DISPOSITION_PATTERN.matcher(mpline);
-                        if (matcher.matches()) {
-                            String attributeString = matcher.group(2);
-                            matcher = CONTENT_DISPOSITION_ATTRIBUTE_PATTERN.matcher(attributeString);
-                            while (matcher.find()) {
-                                String key = matcher.group(1);
-                                if ("name".equalsIgnoreCase(key)) {
-                                    partName = matcher.group(2);
-                                } else if ("filename".equalsIgnoreCase(key)) {
-                                    fileName = matcher.group(2);
-                                    // add these two line to support multiple
-                                    // files uploaded using the same field Id
-                                    if (!fileName.isEmpty()) {
-                                        if (pcount > 0)
-                                            partName = partName + String.valueOf(pcount++);
-                                        else
-                                            pcount++;
-                                    }
+                String partName = null, fileName = null, partContentType = null;
+                // Parse the reset of the header lines
+                mpline = getNextHeaderLine(in);
+                headerLines++;
+                while (mpline != null && mpline.trim().length() > 0) {
+                    Matcher matcher = CONTENT_DISPOSITION_PATTERN.matcher(mpline);
+                    if (matcher.matches()) {
+                        String attributeString = matcher.group(2);
+                        matcher = CONTENT_DISPOSITION_ATTRIBUTE_PATTERN.matcher(attributeString);
+                        while (matcher.find()) {
+                            String key = matcher.group(1);
+                            if ("name".equalsIgnoreCase(key)) {
+                                partName = matcher.group(2);
+                            } else if ("filename".equalsIgnoreCase(key)) {
+                                fileName = matcher.group(2);
+                                // add these two line to support multiple
+                                // files uploaded using the same field Id
+                                if (!fileName.isEmpty()) {
+                                    if (pcount > 0)
+                                        partName = partName + String.valueOf(pcount++);
+                                    else
+                                        pcount++;
                                 }
                             }
                         }
-                        matcher = CONTENT_TYPE_PATTERN.matcher(mpline);
-                        if (matcher.matches()) {
-                            partContentType = matcher.group(2).trim();
-                        }
-                        mpline = in.readLine();
-                        headerLines++;
                     }
-                    int partHeaderLength = 0;
-                    while (headerLines-- > 0) {
-                        partHeaderLength = skipOverNewLine(partHeaderBuff, partHeaderLength);
+                    matcher = CONTENT_TYPE_PATTERN.matcher(mpline);
+                    if (matcher.matches()) {
+                        partContentType = matcher.group(2).trim();
                     }
-                    // Read the part data
-                    if (partHeaderLength >= len - 4) {
-                        throw new ResponseException(Response.Status.INTERNAL_ERROR, "Multipart header size exceeds MAX_HEADER_SIZE.");
-                    }
-                    int partDataStart = boundaryIdxs[boundaryIdx] + partHeaderLength;
-                    int partDataEnd = boundaryIdxs[boundaryIdx + 1] - 4;
-
-                    fbuf.position(partDataStart);
-                    if (partContentType == null) {
-                        // Read the part into a string
-                        String content = getParameterContentsFromBuffer(contentType, fbuf, partDataStart, partDataEnd);
-                        parms.put(partName, content);
-                    } else {
-                        // Read it into a file
-                        String path = saveTmpFile(fbuf, partDataStart, partDataEnd - partDataStart, fileName);
-                        if (!files.containsKey(partName)) {
-                            files.put(partName, path);
-                        } else {
-                            int count = 2;
-                            while (files.containsKey(partName + count)) {
-                                count++;
-                            }
-                            files.put(partName + count, path);
-                        }
-                        parms.put(partName, fileName);
-                    }
+                    mpline = getNextHeaderLine(in);
+                    headerLines++;
                 }
-            } catch (ResponseException re) {
-                throw re;
-            } catch (Exception e) {
+                int partHeaderLength = 0;
+                while (headerLines-- > 0) {
+                    partHeaderLength = skipOverNewLine(partHeaderBuff, partHeaderLength);
+                }
+                // Read the part data
+                if (partHeaderLength >= len - 4) {
+                    throw new ResponseException(Response.Status.INTERNAL_ERROR, "Multipart header size exceeds MAX_HEADER_SIZE.");
+                }
+                int partDataStart = boundaryIdxs[boundaryIdx] + partHeaderLength;
+                int partDataEnd = boundaryIdxs[boundaryIdx + 1] - 4;
+
+                fbuf.position(partDataStart);
+                if (partContentType == null) {
+                    // Read the part into a string
+                    String content = getParameterContentsFromBuffer(contentType, fbuf, partDataStart, partDataEnd);
+                    parms.put(partName, content);
+                } else {
+                    // Read it into a file
+                    String path = saveTmpFile(fbuf, partDataStart, partDataEnd - partDataStart, fileName);
+                    if (!files.containsKey(partName)) {
+                        files.put(partName, path);
+                    } else {
+                        int count = 2;
+                        while (files.containsKey(partName + count)) {
+                            count++;
+                        }
+                        files.put(partName + count, path);
+                    }
+                    parms.put(partName, fileName);
+                }
+            }
+        }
+
+        private String getNextHeaderLine(BufferedReader in) throws ResponseException {
+            try {
+                return in.readLine();
+            } catch (IOException e) {
                 throw new ResponseException(Response.Status.INTERNAL_ERROR, e.toString());
             }
         }
